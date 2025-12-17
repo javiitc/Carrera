76fecb11c7e01b01439d5166c562db1e5202154c
Javi <itsjavii1610@gmail.com>
2025-12-17 10:24:48 +0100

    FIUUUUMMMMMMMMM

--- Java/src/Metodos.java
+++ Java/src/Metodos.java
@@ -37,15 +37,15 @@ public class Metodos {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
 
-        int posicionInicial = random.nextInt(1, 11);
+        int posicionInicial = random.nextInt(1, 16);
         int posicion = posicionInicial;
-        if (posicionInicial <= 3) {
+        if (posicionInicial <= 5) {
             System.out.println(corredorSeleccionado + " ha empezado con buen pie y esta en " + posicionInicial + " lugar!");
             System.out.println("Asegurate de mantener la posición y de ganar la carrera!");
-        } else if (posicionInicial <= 7) {
+        } else if (posicionInicial <= 10) {
             System.out.println(corredorSeleccionado + " no ha comenzado del todo bien y está en " + posicionInicial + " lugar");
             System.out.println("Aún no está todo perdido, puedes asegurarte un buen lugar en la carrera!");
-        } else if (posicionInicial <= 10) {
+        } else if (posicionInicial <= 15) {
             System.out.println(corredorSeleccionado + " ha comenzado de muy mala manera y está en " + posicionInicial + " lugar");
             System.out.println("Aunque todo parezca perdido, aún puedes remontar esta carrera, ve a por ello!");
         }
@@ -54,16 +54,40 @@ public class Metodos {
     }
 
     public static void Carrera(String nombreIngeniero, String corredorSeleccionado, int posicion) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
         System.out.println("Ahora depende de tus instrucciones que " + corredorSeleccionado + " pueda ganar esta carrera. \n" +
                 "Dale las directrices necesarias, pero cuidado, depende que direcciones des puede generar más o menos estrés en " + corredorSeleccionado +"\n" +
                 "Si el nivel de estrés llega a más de 80 " + corredorSeleccionado + " podrá chocarse y la carrera finalizará." );
+
         int nivelEstres = 10;
         boolean carrera = true;
+        String opcion1 = "Paciente";
+        String opcion2 = "Agresivo";
+        String opcion3 = "Temerario";
+
         while (carrera) {
-            System.out.println("Elige la instrucción que quieres darle a " + corredorSeleccionado);
-            System.out.println("1. ");
-            System.out.println("2. ");
-            System.out.println("3. ");
+            System.out.println("Cómo quieres que actúe " + corredorSeleccionado + "?");
+            System.out.println(opcion1);
+            System.out.println(opcion2);
+            System.out.println(opcion3);
+
+            String instruccionElegida = sc.next();
+            int posicionesPerdidas = random.nextInt(0,4);
+            int posicionesGanadas = random.nextInt(0, 3);
+            int posicionesObtenidasTemerarias = random.nextInt(1, 5);
+
+            if (instruccionElegida.equalsIgnoreCase(opcion1)) {
+                if (posicionesPerdidas == 0) {
+                    System.out.println(corredorSeleccionado + " no ha perdido posiciones, se mantiene en " + posicion + " lugar");
+                    System.out.println(corredorSeleccionado + " ha reducido su estrés");
+                } else {
+                    posicion -= posicionesPerdidas;
+                    System.out.println(corredorSeleccionado + " ha sido adelantado y ahora está en " + posicion + " lugar");
+                    System.out.println(corredorSeleccionado + " ha reducido su estrés");
+                }
+            }
         }
     }
 }
