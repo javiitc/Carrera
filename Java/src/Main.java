425cef3584c2d0276eaa1a25634dde1d3e51ca08
Javi <itsjavii1610@gmail.com>
2025-12-17 09:15:58 +0100

    a

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        boolean carrera = true;
         System.out.println("Introduce tu nombre: ");
         String nombreIngeniero = sc.next();
 
         System.out.println("Elige al corredor al que darás instrucciones: ");
-
+        Metodos.nombresCorredores(nombreIngeniero);
     }
 }
--- Java/src/Metodos.java
+++ Java/src/Metodos.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
 
 public class Metodos {
-    int eleccionCorredor;
-
-    public static void nombresCorredores (int eleccionCorredor) {
+    
+    public static void nombresCorredores (String nombreIngeniero) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("1. Hamilton");
-        System.out.println("2. Verstappen");
-        System.out.println("3. Schumacher");
-        System.out.println("4. Fernando Alonso");
+        String nombre1 = "Hamilton";
+        String nombre2 = "Verstappen";
+        String nombre3 = "Schumacher";
+        String nombre4 = "Fernando Alonso";
+        System.out.println("1. " + nombre1);
+        System.out.println("2. " + nombre2);
+        System.out.println("3. " + nombre3);
+        System.out.println("4. " + nombre4);
 
-        eleccionCorredor = sc.nextInt();
+        int eleccionCorredor = sc.nextInt();
 
-        
+        if (eleccionCorredor == 1) {
+            System.out.println(nombre1 + " confía plenamente en tus instrucciones, " + nombreIngeniero);
+        } else if (eleccionCorredor == 2) {
+            System.out.println(nombre2 + " confía plenamente en tus instrucciones, " + nombreIngeniero);
+        } else if (eleccionCorredor == 3) {
+            System.out.println(nombre3 + " confía plenamente en tus instrucciones, " + nombreIngeniero);
+        } else {
+            System.out.println(nombre4 + " confía plenamente en tus instrucciones, " + nombreIngeniero);
+        }
     }
+
 }
