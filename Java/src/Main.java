b030f31fda0b08aca158239fb3ec2cb9400dccad
Javi <itsjavii1610@gmail.com>
2026-02-03 18:26:55 +0100

    finalizado

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -15,6 +15,8 @@ public class Main {
         System.out.println("   INICIO DE CARRERA   ");
         System.out.println("=======================");
 
-        Metodos.inicioCarrera(nombreIngeniero, corredorSeleccionado);
+        int posicion = Metodos.inicioCarrera(nombreIngeniero, corredorSeleccionado);
+
+        Metodos.Carrera(nombreIngeniero, corredorSeleccionado, posicion);
     }
 }
